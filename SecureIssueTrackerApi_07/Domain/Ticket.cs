02eacf05ee832f135dd76aca82afcf99b51370c8
jose Angel <josegallardo.cor@gmail.com>
2026-08-28 12:54:54 -0600

    feat:  entidades de dominio User y Ticket

---
+++ SecureIssueTrackerApi_07/Domain/Ticket.cs
@@ -0,0 +1,73 @@
+﻿using SecureIssueTrackerApi_07.Exceptions;
+using System.Data;
+
+namespace SecureIssueTrackerApi_07.Domain
+{
+    public class Ticket
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public TicketStatus Status { get; private set; }
+        public TicketPriority PriorityId { get; private set; }
+        public Guid CreatedByUserId { get; private set; }
+        public Guid AssignedToUserId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public DateTime? ClosedAt { get; private set; }
+
+        public User CreatedByUser { get; private set; } = null!;
+        public User AssignedToUser { get; private set; } = null!;
+
+        private Ticket() { }
+
+        public Ticket(string title, string desciption, TicketPriority priorityId, Guid createdByUserId)
+        {
+            Validate(title, desciption, priorityId, createdByUserId);
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = desciption;
+            Status = TicketStatus.Open;
+            PriorityId = priorityId;
+            CreatedByUserId = createdByUserId;
+            CreatedAt = DateTime.UtcNow;
+        }
+        public void UpdateDescripcion(string desciption)
+        {
+            if (string.IsNullOrWhiteSpace(desciption)) throw new DomainException("La descripcion no puede ser null o estar vacia.");
+            Description = desciption;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void Open()
+        {
+            if (Status == TicketStatus.Closed) throw new DomainException("Un ticket cerrado no puede reabrirse en esta version.");
+            Status = TicketStatus.InProgress;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void InProgress()
+        {
+            if (Status != TicketStatus.Open) throw new DomainException("Solo un ticket abierto puede pasar a estar en progreso.");
+            Status = TicketStatus.InProgress;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void Resolved()
+        {
+            if (Status != TicketStatus.InProgress) throw new DomainException("Solo un ticket en progreso puede ser resulto.");
+            Status = TicketStatus.Resolved;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void Closed()
+        {
+            if (Status != TicketStatus.Resolved) throw new DomainException("Solo un ticket resuelto puede ser cerado.");
+            Status = TicketStatus.Closed;
+            ClosedAt = DateTime.UtcNow;
+        }
+        private void Validate(string title, string desciption, TicketPriority priorityId, Guid createdByUserId)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new DomainException("El titulo no puede ser null o estar vacio.");
+            if (string.IsNullOrWhiteSpace(desciption)) throw new DomainException("La descripcion no puede ser null o estar vacia.");
+            if (!Enum.IsDefined(typeof(TicketPriority), priorityId)) throw new DomainException("La prioridad no es valida.");
+            if (createdByUserId == Guid.Empty) throw new DomainException("El Id de usuario de creacion no puede estar vacio.");
+        }
+    }
+}
---
+++ SecureIssueTrackerApi_07/Domain/User.cs
@@ -0,0 +1,45 @@
+﻿using SecureIssueTrackerApi_07.Exceptions;
+
+namespace SecureIssueTrackerApi_07.Domain
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+        public UserRole Role { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public ICollection<Ticket> Tickets { get; } = new List<Ticket>();
+        private User() { }
+
+        public User(string fullName, string email, string passwordHash, UserRole role)
+        {
+            Validate(fullName,email,passwordHash,role);
+            Id = Guid.NewGuid();
+            FullName = fullName;
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = role;
+            IsActive = true;
+            CreatedAt = DateTime.Now;
+        }
+        public void Activate()
+        {
+            IsActive = true;
+        }
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+        private void Validate(string fullName, string email, string passwordHash, UserRole role)
+        {
+            if (String.IsNullOrWhiteSpace(fullName)) throw new DomainException("El nombre no puede ser null o estar vacio.");
+            if (String.IsNullOrWhiteSpace(email)) throw new DomainException("El correo no puede ser null o estar vacio.");
+            if (String.IsNullOrWhiteSpace(passwordHash)) throw new DomainException("La contraseña no puede ser null o estar vacia.");
+            if (!Enum.IsDefined(typeof(UserRole), role)) throw new DomainException("El role no es valido.");
+        }
+    }
+}
