5885f1bd2dd2ffb748b3af2058fc48b2b788fa22
jose Angel <josegallardo.cor@gmail.com>
2026-08-30 13:23:04 -0600

    feat: casos de uso para consultas de ticktes

--- SecureIssueTrackerApi_07/Application/TicketUserCase.cs
+++ SecureIssueTrackerApi_07/Application/TicketUserCase.cs
@@ -3,6 +3,7 @@ using SecureIssueTrackerApi_07.Domain;
 using SecureIssueTrackerApi_07.Dtos.Ticket;
 using SecureIssueTrackerApi_07.Exceptions;
 using SecureIssueTrackerApi_07.Infrastructure;
+using System.Net.Sockets;
 
 namespace SecureIssueTrackerApi_07.Application
 {
@@ -89,6 +90,83 @@ namespace SecureIssueTrackerApi_07.Application
             ticket.AssignTo(userId);
             await _context.SaveChangesAsync();
         }
+        public async Task<TicketDto> GetById(Guid id)
+        {
+            var ticket = await _context.Tickets.AsNoTracking()
+                .Include(u => u.CreatedByUser)
+                .Include(u => u.AssignedToUser)
+                .FirstOrDefaultAsync(u => u.Id == id);
+            if (ticket == null) throw new NotFoundException("El ticket no esta en el sistema.");
+            return new TicketDto
+            {
+                Id = ticket.Id,
+                Title = ticket.Title,
+                Description = ticket.Description,
+                Status = ticket.Status,
+                Priority = ticket.Priority,
+                CreatedByUserId = ticket.CreatedByUserId,
+                CreatedByUserName = ticket.CreatedByUser.FullName,
+                AssignedToUserId = ticket.AssignedToUserId,
+                AssignedToUserName = ticket.AssignedToUser != null ? ticket.AssignedToUser.FullName : "",
+                CreatedAt = ticket.CreatedAt,
+                UpdatedAt = ticket.UpdatedAt,
+                ClosedAt = ticket.ClosedAt
+            };
+        }
+        public async Task<IEnumerable<TicketDto>> GetAll(TicketQuery paramsQuery)
+        {
+            IQueryable<Ticket> query = _context.Tickets.AsNoTracking()
+                .Include(u => u.CreatedByUser)
+                .Include(u => u.AssignedToUser);
 
+            if (String.IsNullOrWhiteSpace(paramsQuery.Title))
+            {
+                query = query.Where(t => t.Title.ToLower().Contains(paramsQuery.Title!));
+            }
+            if (String.IsNullOrWhiteSpace(paramsQuery.Description))
+            {
+                query = query.Where(t => t.Description.ToLower().Contains(paramsQuery.Description!));
+            }
+            if (paramsQuery.Status.HasValue)
+            {
+                query = query.Where(u => u.Status == paramsQuery.Status.Value);
+            }
+            if (paramsQuery.Priority.HasValue)
+            {
+                query = query.Where(u => u.Priority == paramsQuery.Priority.Value);
+            }
+            if (paramsQuery.CreatedByUserId.HasValue)
+            {
+                query = query.Where(u => u.CreatedByUserId == paramsQuery.CreatedByUserId.Value);
+            }
+            if (paramsQuery.AssignedToUserId.HasValue)
+            {
+                query = query.Where(u => u.AssignedToUserId == paramsQuery.AssignedToUserId.Value);
+            }
+            if (paramsQuery.FromDate.HasValue)
+            {
+                query = query.Where(u => u.CreatedAt >= paramsQuery.FromDate.Value);
+            }
+            if (paramsQuery.ToDate.HasValue)
+            {
+                query = query.Where(u => u.CreatedAt <= paramsQuery.ToDate.Value);
+            }
+
+            return await query.Select(ticket => new TicketDto
+            {
+                Id = ticket.Id,
+                Title = ticket.Title,
+                Description = ticket.Description,
+                Status = ticket.Status,
+                Priority = ticket.Priority,
+                CreatedByUserId = ticket.CreatedByUserId,
+                CreatedByUserName = ticket.CreatedByUser.FullName,
+                AssignedToUserId = ticket.AssignedToUserId,
+                AssignedToUserName = ticket.AssignedToUser != null ? ticket.AssignedToUser.FullName : "",
+                CreatedAt = ticket.CreatedAt,
+                UpdatedAt = ticket.UpdatedAt,
+                ClosedAt = ticket.ClosedAt
+            }).ToArrayAsync();
+        }
     }
 }
--- SecureIssueTrackerApi_07/Dtos/Ticket/TicketQuery.cs
+++ SecureIssueTrackerApi_07/Dtos/Ticket/TicketQuery.cs
@@ -1,6 +1,16 @@
-﻿namespace SecureIssueTrackerApi_07.Dtos.Ticket
+﻿using SecureIssueTrackerApi_07.Domain;
+
+namespace SecureIssueTrackerApi_07.Dtos.Ticket
 {
     public class TicketQuery
     {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public TicketStatus? Status { get; set; }
+        public TicketPriority? Priority { get; set; }
+        public Guid? CreatedByUserId { get; set; }
+        public Guid? AssignedToUserId { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
     }
 }
