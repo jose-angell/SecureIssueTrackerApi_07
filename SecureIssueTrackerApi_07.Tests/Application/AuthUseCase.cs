97a18d6cf5ebd6fbfb1f0c953cbb34d2e4d3e251
jose Angel <josegallardo.cor@gmail.com>
2026-09-08 23:41:35 -0600

    feat: creacion de proyecto de pruebas y configuracion para pruebas unitarias

---
+++ SecureIssueTrackerApi_07.Tests/Application/AuthUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureIssueTrackerApi_07.Tests.Application
+{
+    public class AuthUseCase
+    {
+
+    }
+}
---
+++ SecureIssueTrackerApi_07.Tests/TestDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SecureIssueTrackerApi_07.Infrastructure;
+
+namespace SecureIssueTrackerApi_07.Tests
+{
+    public class TestDbContextFactory : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+
+        public AppDbContext Context { get; }
+
+        public TestDbContextFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            Context = new AppDbContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+            _connection.Dispose();
+        }
+    }
+}
